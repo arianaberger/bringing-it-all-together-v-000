50bce3f699178fa0a93e1055bcb2a5bfe0a065d7
Ariana Berger <ariana.berger@gmail.com>
2018-03-22 23:07:16 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Dog
 
   attr_accessor :name, :breed
@@ -54,6 +56,7 @@ class Dog
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else
       dog = self.create(name: name, breed: breed)
+      binding.pry
     end
     dog
   end
